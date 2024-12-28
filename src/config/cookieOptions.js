52da550d29582849ed7935f152d9fd9c612502d6
godspeed-03 <satyam2001anand@gmail.com>
2024-12-28 18:42:44 +0530

    cokkie option modified

--- src/config/cookieOptions.js
+++ src/config/cookieOptions.js
@@ -1,13 +1,13 @@
 export const accessTokenOptions = {
   httpOnly: true,
   secure: true,
-  sameSite: "none",
+  // sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000, 
 };
   
 export const refreshTokenOptions = {
   httpOnly: true,
   secure: true,
-  sameSite: "none",
+  // sameSite: "none",
   maxAge: 30 * 24 * 60 * 60 * 1000, 
 };
